442280ec4e50af09fd806aba4b0e6ebaa5d6c4d8
sunil.patel <sunil.patel@artoon.in>
2021-02-15 14:07:05 +0530

    Initial commit

--- js/custome.js
+++ js/custome.js
@@ -8,6 +8,20 @@ $(document).ready(function() {
         $("#myTabContent").removeClass("wa-tab-c-effect");
     });
 
+
+
+    // who-we-are-section
+    let waAboutFounder = document.querySelector('#wa-about-founder');
+    let whoWeAreBack = document.querySelector('#who-we-are-back');
+    waAboutFounder.addEventListener('click', () => {
+        $('.wa-wwa').css({ 'left': '-100vw', 'position': 'relative', 'transition': '1s' });
+        $('#wa-wwa-section').addClass('wwa-section');
+    });
+    whoWeAreBack.addEventListener('click', () => {
+        $('.wa-wwa').css({ 'left': '0vw' });
+        $('#wa-wwa-section').removeClass('wwa-section');
+    });
+
 });
 
 
@@ -19,3 +33,19 @@ $(window).scroll(function() {
         $('nav').removeClass('ft-bg');
     }
 });
+
+
+// Slider
+(function() {
+    let slider = document.querySelector('.slider');
+    let slides = document.querySelector('.slides');
+    let slide = document.querySelectorAll('.slide').length;
+    let counter = 1;
+
+    setInterval(function() {
+
+    });
+
+
+
+})();
